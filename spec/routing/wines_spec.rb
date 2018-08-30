706c985811f57b82d617d789211af6c7dd2cb9a1
mdipietr0 <dipietro.michael@gmail.com>
2018-08-30 14:45:55 -0400

    Add passing unit test for delete /wines/:id route
    - Add route for delete /wines/:id
    - Add unit test for mapping to destroy action

--- spec/routing/wines_spec.rb
+++ spec/routing/wines_spec.rb
@@ -4,53 +4,33 @@ require 'rails_helper'
 
 RSpec.describe 'routes for wines' do
   it 'routes GET /wines to the wines#index action' do
+    # check that a get request to /articles maps the routes to the index method
+    # in the articles controller
     expect(get('/wines')).to route_to('wines#index')
     expect(get: '/wines').to route_to(controller: 'wines', action: 'index')
   end
 
   it 'routes GET /wines/:id to the wines#show action' do
+    # check that a get request to /articles/:id maps to the show action
+    # in the articles controller, with the ID that came in the request
     expect(get('/wines/1')).to route_to(
       controller: 'wines',
       action: 'show',
       id: '1'
     )
   end
+
+  it 'routes DELETE /wines/:id to the wines#destroy action' do
+    # check that a delete request to /articles/:id maps to the destroy
+    # action in the articles controller, with the ID that came in the request
+    expect(delete('/wines/1')).to route_to(
+      controller: 'wines',
+      action: 'destroy',
+      id: '1'
+    )
+  end
 end
 
-#
-#
-#
-#
-#
-# require 'rails_helper'
-#
-# RSpec.describe 'routes for articles' do
-#   it 'routes GET /articles to the articles#index action' do
-#     # check that a get request to /articles maps the routes to the index method
-#     # in the articles controller
-#     expect(get('/articles')).to route_to('articles#index')
-#   end
-#
-#   it 'routes GET /articles/:id to the articles#show action' do
-#     # check that a get request to /articles/:id maps to the show action
-#     # in the articles controller, with the ID that came in the request
-#     expect(get('/articles/1')).to route_to(
-#       controller: 'articles',
-#       action: 'show',
-#       id: '1'
-#     )
-#   end
-#
-#   it 'routes DELETE /articles/:id to the articles#destroy action' do
-#     # check that a delete request to /articles/:id maps to the destroy
-#     # action in the articles controller, with the ID that came in the request
-#     expect(delete('/articles/1')).to route_to(
-#       controller: 'articles',
-#       action: 'destroy',
-#       id: '1'
-#     )
-#   end
-#
 #   it 'routes PATCH /articles/:id to the articles#update action' do
 #     # check that a patch request to /articles/:id maps to the patch
 #     # action in the articles controller, with the ID that came in the request
